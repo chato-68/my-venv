b21f0f7676d42656b854ed9ef69c75df9bd4af36
chato-68 <117103586+chato-68@users.noreply.github.com>
2022-10-31 20:15:22 -0400

    clase1.1: se agrego a dinosaur

---
+++ CDCF-Dino-Runner-Grupo1-master/CDCF-Dino-Runner-Grupo1-master/dino_runner/components/dinosuar.py
@@ -0,0 +1,24 @@
+from urllib.parse import SplitResultBytes
+
+
+import pygame
+from pygame.sprite import Sprite
+
+from utils.constants import RUNNING
+
+class Dinosaur(Sprite):
+    def __init__(self):
+        self.image = RUNNING[0]
+        self.image_rect = self.image.get_rect()
+        self.image_rect,x = 50
+        self.image_rect.y = 300
+        self.step = 0
+    
+    def update(self):
+        self.image = RUNNING[0] if self.step <=5 else RUNNING[1]
+        self.image_rect = self.image.get_rect()
+        self.image_rect.x = 50
+        self.image_rect.y = 300
+        self.step += 1
+
+        if self.step >
